1da563cfa9eae72113c6036f6348703132fed8f0
MS Kim(tony9402) <tony9402@naver.com>
2022-10-05 03:35:44 +0900

    [UPD] 주석 표시 변경 및 솔루션 코드 정리

--- prefix_sum/11441/main.py
+++ prefix_sum/11441/main.py
@@ -1,6 +1,6 @@
-// Authored by : vswngjs
-// Co-authored by : -
-// Link : http://boj.kr/a2fb7c4e954d42f4bfabc04bbd2973ba
+# Authored by : vswngjs
+# Co-authored by : tony9402
+# Link : http://boj.kr/a2fb7c4e954d42f4bfabc04bbd2973ba
 import sys
 
 def input():
@@ -9,8 +9,7 @@ def input():
 n = int(input())
 
 numbers = list(map(int, input().split(" ")))
-solve_list = []                             # 누적합을 저장하기 위한 리스트
-
+solve_list = [0] # 누적합을 저장하기 위한 리스트
 sum = 0
 
 for i in numbers:
@@ -18,10 +17,6 @@ for i in numbers:
     solve_list.append(sum)
 
 testcase = int(input())
-
 for i in range(testcase):
     parts = list(map(int, input().split()))
-    if parts[0] == 1:                       # 처음부터 j 인덱스까지의 누적합
-        print(solve_list[parts[1] - 1])
-    else:
-        print(solve_list[parts[1] - 1] - solve_list[parts[0] - 2])  # i ~ j 인덱스까지의 누적합
+    print(solve_list[parts[1]] - solve_list[parts[0] - 1])  # i ~ j 인덱스까지의 누적합
