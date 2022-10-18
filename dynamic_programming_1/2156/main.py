c69080507b879e990bd5844fb09f7941542f15ff
MS Kim(tony9402) <tony9402@naver.com>
2022-10-19 06:39:09 +0900

    [UPD] Refactoring

--- dynamic_programming_1/2156/main.py
+++ dynamic_programming_1/2156/main.py
@@ -1,19 +1,17 @@
 # Authored by : wassup37
-# Co-authored by : -
-# Link : http://boj.kr/7ae60e4f30294ef5bf67bbf30ba41ddf
+# Co-authored by : tony9402
+# Link : http://boj.kr/1211037748274f97ae2a8a24c4f0ec9e
 import sys
 
-read = lambda : sys.stdin.readline().rstrip()
-n = int(read())
-wine = list()
-for _ in range(n):
-    wine.append(int(read()))
-dp = list() # n - 1 인덱스까지의 최댓값을 저장하는 리스트
-dp.append(wine[0])
-if n > 1:
-    dp.append(wine[0] + wine[1])
-if n > 2:
-    dp.append(max(dp[1], dp[0] + wine[2], wine[1] + wine[2]))
-for i in range(3, n):   # 규칙성을 찾아 3가지 case에 대한 최댓값을 삽입한다
-    dp.append(max(dp[i - 1], dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]))
-print(dp[n - 1])    # 마지막 인덱스까지 확인해서 출력
+def input():
+    return sys.stdin.readline().rstrip()
+N = int(input())
+wine = [0] + [int(input()) for i in range(N)] # 인덱스가 1부터 시작하도록 만듭니다.
+
+if N == 1: # n이 1일 경우 맨 처음 포도주를 마시는게 최대입니다.
+    print(wine[1])
+else:
+    dp = [0, wine[1], wine[1] + wine[2]] # n 인덱스까지의 최댓값을 저장하는 리스트
+    for i in range(3, N+1): # 3부터 n까지 dp 테이블을 채웁니다.
+        dp.append(max(dp[i - 1], dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]))
+    print(dp[N]) # 포도주 잔의 개수가 n개일 때, 최대로 마실 수 있는 포도주의 양 출력
