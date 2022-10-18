da36531e5da166c2897f9e990d42651685c3c309
MS Kim(tony9402) <tony9402@soongsil.ac.kr>
2022-10-19 06:46:58 +0900

    [UPD] Refactoring

--- data_structure/4949/main.cpp
+++ data_structure/4949/main.cpp
@@ -1,30 +1,24 @@
 // Authored by : jminryu
 // Co-authored by : -
 // Link : http://boj.kr/1f1b7257c19e45169d3c4384b315ec7f
-
 #include <bits/stdc++.h>
 using namespace std;
-
-stack<char> st;
-bool isBalanced = false;
-string str;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    string str;
     while (1) {
         getline(cin, str);
-        
         if (str == ".") break;
         
-        isBalanced = true;
-        st = {};
+        bool isBalanced = true;
+        stack<char> st;
         
         for (char c : str) {
             if (!isBalanced) break;
-            
             switch (c) {
                 case '(':
                     st.push(c);
@@ -44,10 +38,8 @@ int main() {
                     break;
             }
         }
-        
         if (isBalanced && st.empty()) cout << "yes\n";
         else cout << "no\n";
     }
-
     return 0;
 }
