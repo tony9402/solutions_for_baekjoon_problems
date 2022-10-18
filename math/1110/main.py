19ce1067cf9bf39b23402f2c21a82aedfe0e62b4
MS Kim(tony9402) <tony9402@soongsil.ac.kr>
2022-10-19 06:51:20 +0900

    [UPD] Refactoring

--- math/1110/main.py
+++ math/1110/main.py
@@ -1,7 +1,6 @@
 # Authored by : gyeong_sun
 # Co-authored by : -
 # Link : http://boj.kr/8710e4702aa04dfa94ba38bd8cb271dd 
-
 import sys
 
 def input():
@@ -12,15 +11,12 @@ num = origin_num
 cycle = 0             #반복 사이클 수
 
 while True:
-    n1 = num //10    #십의 자리
-    n2 = num % 10    #일의 자리 
-    n3 = (n1+n2) % 10    
-
-    num = n2 * 10 + n3
-
-    cycle +=1
-
+    n1 = num//10    #십의 자리
+    n2 = num%10     #일의 자리 
+    n3 = (n1+n2)%10
+    num = n2*10+n3
+    cycle += 1
     if origin_num == num:     #원래 수로 돌아온 경우
         break                 #반복문 빠져나감
-    
+
 print(cycle)
