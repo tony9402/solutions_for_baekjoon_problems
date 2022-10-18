1b412e9c7678f604b530baae30ad4e5a938843a8
MS Kim(tony9402) <tony9402@naver.com>
2022-10-19 07:02:52 +0900

    [UPD] Refactoring

--- dynamic_programming_1/2156/Main.java
+++ dynamic_programming_1/2156/Main.java
@@ -3,6 +3,7 @@
 // Link : http://boj.kr/a324587d69454b0b9ecca053e1799831
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String [] args) {
         FastReader rd = new FastReader();
@@ -14,19 +15,21 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             grapes[i] = rd.nextInt();
         }
-
-        dp[1] = grapes[1];
-        if (N > 1) {
+        
+        if(N == 1) {
+            System.out.println(grapes[1]);
+        }
+        else {
+            dp[1] = grapes[1];
             dp[2] = grapes[1] + grapes[2];
+            for (int i = 3; i <= N; i++) {
+                dp[i] = Math.max(dp[i - 2] + grapes[i], dp[i - 3] + grapes[i - 1] + grapes[i]);
+                dp[i] = Math.max(dp[i], dp[i - 1]);
+            }
+            System.out.println(dp[N]);
         }
-        for (int i = 3; i <= N; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + grapes[i], dp[i - 3] + grapes[i - 1] + grapes[i]));
-        }
-        System.out.println(dp[N]);
-
-
-
     }
+
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
