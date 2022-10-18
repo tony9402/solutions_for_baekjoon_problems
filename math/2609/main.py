c2e450bb6a2a9a210221ff544645318bc58feef1
MS Kim(tony9402) <tony9402@naver.com>
2022-10-19 06:49:07 +0900

    [UPD] Refactoring

--- math/2609/main.py
+++ math/2609/main.py
@@ -1,16 +1,15 @@
 # Authored by : hji7599
 # Co-authored by : -
 # Link : http://boj.kr/9f0533b8217348959a820f3ac7f19283
-
-# Euclidean algorithm
 import sys
 
 def input():
     return sys.stdin.readline().rstrip()
 
+# 유클리드 호제법(Euclidean algorithm)
 def gcd(num1, num2):
     if num2 == 0: return num1
-    else: return gcd(num2, num1 % num2)
+    return gcd(num2, num1 % num2)
 
 def lcm(num1, num2):
     return (num1 * num2) // gcd(num1, num2)
